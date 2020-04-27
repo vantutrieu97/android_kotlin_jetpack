f1895aba2b45f44fc6a4fecb7c3c5591a58f6b8b
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-28 03:02:35 +0700

    Clean up

--- app/src/main/java/vantutrieu97/myapplication/views/DetailFragment.kt
+++ app/src/main/java/vantutrieu97/myapplication/views/DetailFragment.kt
@@ -5,9 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.fragment_detail.*
 import vantutrieu97.myapplication.R
 
 /**
@@ -21,20 +18,5 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        arguments?.let {
-            dogUid = DetailFragmentArgs.fromBundle(it).dogUid
-            textView.text = dogUid.toString()
-        }
-        listBtn.setOnClickListener {
-            if (it.findNavController().currentDestination?.id == R.id.detailFragment) {
-                var action = DetailFragmentDirections.actionListFragment()
-                Navigation.findNavController(it).navigate(action)
-            }
-        }
     }
 }
--- app/src/main/java/vantutrieu97/myapplication/views/ListFragment.kt
+++ app/src/main/java/vantutrieu97/myapplication/views/ListFragment.kt
@@ -5,9 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.fragment_list.*
 import vantutrieu97.myapplication.R
 
 /**
@@ -21,16 +18,4 @@ class ListFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        detailBtn.setOnClickListener {
-            if (it.findNavController().currentDestination?.id == R.id.listFragment) {
-                var action = ListFragmentDirections.actionDetailFragment(10)
-                Navigation.findNavController(it).navigate(action)
-            }
-        }
-    }
-
-
 }
