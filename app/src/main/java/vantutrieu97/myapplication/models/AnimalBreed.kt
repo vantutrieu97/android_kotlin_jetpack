7fff40b80d069d44c08b0760afdb169803b02546
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-28 13:33:30 +0700

    Create AnimalBreed.kt

---
+++ app/src/main/java/vantutrieu97/myapplication/models/AnimalBreed.kt
@@ -0,0 +1,11 @@
+package vantutrieu97.myapplication.models
+
+data class AnimalBreed(
+    val breedId: String?,
+    val dogBreed: String?,
+    val lifeSpan: String?,
+    val breedGroup: String?,
+    val breedFor: String?,
+    val temperament: String?,
+    val imageUrl: String?
+)
