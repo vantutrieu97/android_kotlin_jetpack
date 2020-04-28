cac419c0381cda3eb02323f1a54ae745b0a7ec09
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-29 02:50:50 +0700

    Update Animal.kt

--- app/src/main/java/vantutrieu97/myapplication/models/Animal.kt
+++ app/src/main/java/vantutrieu97/myapplication/models/Animal.kt
@@ -1,11 +1,20 @@
 package vantutrieu97.myapplication.models
 
+import com.google.gson.annotations.SerializedName
+
 data class Animal(
+    @SerializedName("id")
     val breedId: String?,
+    @SerializedName("name")
     val breed: String?,
+    @SerializedName("life_span")
     val lifeSpan: String?,
+    @SerializedName("breed_group")
     val breedGroup: String?,
+    @SerializedName("bred_for")
     val breedFor: String?,
+    @SerializedName("temperament")
     val temperament: String?,
+    @SerializedName("url")
     val imageUrl: String?
 )
