2080acda5672c1b380e3a7b6e58f75a55c1b7207
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-30 17:49:27 +0700

    Create Util.kt

---
+++ app/src/main/java/vantutrieu97/myapplication/utils/Util.kt
@@ -0,0 +1,27 @@
+package vantutrieu97.myapplication.utils
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import vantutrieu97.myapplication.R
+
+
+fun getProgressDrawable(context: Context): CircularProgressDrawable {
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 10f
+        centerRadius = 50f
+        start()
+    }
+}
+
+fun ImageView.loadImage(url: String?, progressDrawable: CircularProgressDrawable) {
+    val option = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.mipmap.ic_launcher)
+    Glide.with(context)
+        .setDefaultRequestOptions(option)
+        .load(url)
+        .into(this)
+}
