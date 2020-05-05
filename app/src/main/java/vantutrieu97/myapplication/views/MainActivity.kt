6892a6c434e2b9b96a1a6929330dd41aa9f4fb92
vantutrieu97 <vantutrieu97@gmail.com>
2020-05-06 02:32:31 +0700

    Show dark action bar

--- app/src/main/java/vantutrieu97/myapplication/views/MainActivity.kt
+++ app/src/main/java/vantutrieu97/myapplication/views/MainActivity.kt
@@ -3,11 +3,15 @@ package vantutrieu97.myapplication.views
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 import vantutrieu97.myapplication.R
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    //    private lateinit var navController: NavController
+    private lateinit var navController: NavController
     private val TAG = "CHECK"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,9 +27,14 @@ class MainActivity : AppCompatActivity() {
         Log.i(TAG, "onStart:${Calendar.getInstance().getTime()}")
 //        navController = Navigation.findNavController(this, R.id.fragmentContainerViewTag)
 //        NavigationUI.setupActionBarWithNavController(this, navController)
+        var navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerViewTag) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
-//    override fun onSupportNavigateUp(): Boolean {
-////        return NavigationUI.navigateUp(navController, null)
-//    }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
 }
