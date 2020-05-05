65a508d5c2e7b48947643579d5bf2db14a28a13d
vantutrieu97 <vantutrieu97@gmail.com>
2020-05-06 01:52:55 +0700

    Change app theme
    DarkActionBar -> NoActionBar

--- app/src/main/java/vantutrieu97/myapplication/views/MainActivity.kt
+++ app/src/main/java/vantutrieu97/myapplication/views/MainActivity.kt
@@ -3,14 +3,11 @@ package vantutrieu97.myapplication.views
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
 import vantutrieu97.myapplication.R
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
+    //    private lateinit var navController: NavController
     private val TAG = "CHECK"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,11 +21,11 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         Log.i(TAG, "onStart:${Calendar.getInstance().getTime()}")
-        navController = Navigation.findNavController(this, R.id.fragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+//        navController = Navigation.findNavController(this, R.id.fragmentContainerViewTag)
+//        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, null)
-    }
+//    override fun onSupportNavigateUp(): Boolean {
+////        return NavigationUI.navigateUp(navController, null)
+//    }
 }
